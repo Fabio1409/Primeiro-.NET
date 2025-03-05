8838751d66cb2498750e0c469df18a3a3fd9d406
Jose Fabio <fabio.7.45@hotmail.com>
2025-03-05 00:32:53 -0300

    Simple create form

--- Primeiro.NET/Controllers/SellersController.cs
+++ Primeiro.NET/Controllers/SellersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PrimeiroDOTNET.Services;
+using PrimeiroDOTNET.Models;
 
 namespace PrimeiroDOTNET.Controllers
 {
@@ -15,6 +16,18 @@ namespace PrimeiroDOTNET.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
